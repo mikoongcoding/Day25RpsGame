45c5a1e27cf32762579da30f7226e1c53b536fdb
mikoongcoding <skymikong@gmail.com>
2022-01-15 22:45:41 +0900

    Initial Commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Mi Gyeong Park on 2022/01/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Mi Gyeong Park on 2022/01/15.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
